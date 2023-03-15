fad0f39fb0fd7bfd6fcc81c4098f1e7e143a9df4
Ank7raj7 <127223712+Ank7raj7@users.noreply.github.com>
2023-03-15 16:27:40 +0530

    Add files via upload

---
+++ Mad Lib.py
@@ -0,0 +1,13 @@
+#user input
+name=input("What is your name?: ")
+time1=input("Give me a time:")
+time2=input("Give me another time: ")
+pronoun=input("Give me a pronoun: ")
+noun1=input("Give me a noun: ")
+noun2=input("Give me a noun:")
+verb=input("Give me a (past tense) verb:")
+#actual statement
+statement= f"Hi! My name is {name}.I woke up at {time1} to go to school,but school starts at {time2}.\
+After walking up {pronoun}.I decided to go get ready ! I changed into my favouritr outfit- adressy pant.\
+and a {noun1}.Then it was time for me to eat my go to breakfast is a {noun2}.I then {verb} to school."
+print(statement)
